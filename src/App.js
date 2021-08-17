bf932f159db3f3dacb665bc0211e2855f9d0fed2
jimmycababa <jimmycababa@gmail.com>
2021-08-16 23:01:41 -0500

    messing around with the grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import PeriodicTable from './PeriodicTable';
 
 function App() {
   return (
     <div className="App">
       <h1>Periodic Table of Pasta</h1>
+      <PeriodicTable />
     </div>
   );
 }
