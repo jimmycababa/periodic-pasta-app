e6b5c69f7ada7dae3123a4e86ffc1fe029f6554c
Scott Gorsky <scottgorsky@Scott-Gorskys-MacBook-Pro.local>
2021-08-24 15:41:02 -0500

    testing pasta data

---
+++ src/pastaData.js
@@ -0,0 +1,17 @@
+module.exports = {
+  pastas: [
+    {
+      name: 'None',
+      summary: 'Place of zero so that the array starts with 1 with hydrogen'
+    },
+    {
+      name: 'Spaghetti',
+      shape: 'long thin cylinder',
+      boil: 20.271,
+      source: 'https://en.wikipedia.org/wiki/Spaghetti',
+      spectral_img: 'https://en.wikipedia.org/wiki/File:Hydrogen_Spectra.jpg',
+      symbol: 'S',
+      xpos: 1,
+      ypos: 1,
+      shells: [1]
+    },
