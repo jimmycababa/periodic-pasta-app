286b9f92f4a5b1c35ba11c306867d7b4e0bdb94a
Scott Gorsky <scottgorsky@Scott-Gorskys-MacBook-Pro.local>
2021-08-30 14:52:30 -0500

    first addition of data

--- src/pastaData.js
+++ src/pastaData.js
@@ -7,11 +7,56 @@ module.exports = {
     {
       name: 'Spaghetti',
       shape: 'long thin cylinder',
-      boil: 20.271,
+      translation: 'thin string'
       source: 'https://en.wikipedia.org/wiki/Spaghetti',
-      spectral_img: 'https://en.wikipedia.org/wiki/File:Hydrogen_Spectra.jpg',
       symbol: 'S',
       xpos: 1,
       ypos: 1,
-      shells: [1]
     },
+    {
+      name: 'Linguine',
+      shape: 'long elliptical noodle',
+      translation: 'little tongues'
+      source: 'https://en.wikipedia.org/wiki/Linguine',
+      symbol: 'Li',
+      xpos: 1,
+      ypos: 2,
+    },
+    {
+      name: 'Fettuccine',
+      shape: 'flat thick noodle',
+      translation: 'little ribbons'
+      source: 'https://en.wikipedia.org/wiki/Fettuccine',
+      symbol: 'Fe',
+      xpos: 1,
+      ypos: 3,
+    },
+    {
+      name: 'Bucatini',
+      shape: 'spaghetti-like with a hole through the center',
+      translation: 'hole'
+      source: 'https://en.wikipedia.org/wiki/Bucatini',
+      symbol: 'Bu',
+      xpos: 1,
+      ypos: 4,
+    },
+    {
+      name: 'Capellini',
+      shape: 'very thin spaghetti',
+      translation: 'little hairs'
+      source: 'https://en.wikipedia.org/wiki/Capellini',
+      symbol: 'Ca',
+      xpos: 1,
+      ypos: 5,
+    },
+    {
+      name: 'Pappardelle',
+      shape: 'broad flat noodles',
+      translation: 'to gobble up'
+      source: 'https://en.wikipedia.org/wiki/Pappardelle',
+      symbol: 'Pa',
+      xpos: 1,
+      ypos: 6,
+    },
+  ]
+}
